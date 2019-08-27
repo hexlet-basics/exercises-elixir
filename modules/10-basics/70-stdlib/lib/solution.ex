eab80fbb19e7552d5dda6b2b4a6776edcb04944f
Kirill Mokevnin <mokevnin@gmail.com>
2019-08-27 10:05:00 -0400

    add structure

---
+++ modules/10-basics/70-stdlib/lib/solution.ex
@@ -0,0 +1,5 @@
+defmodule Solution do
+  def hello do
+    IO.puts("Hello, World!")
+  end
+end
---
+++ modules/10-basics/70-stdlib/test/solution_test.exs
@@ -0,0 +1,9 @@
+defmodule Test do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "greets the world" do
+    msg = capture_io(fn -> Solution.hello() end) |> String.trim()
+    assert msg == "Hello, World!"
+  end
+end
