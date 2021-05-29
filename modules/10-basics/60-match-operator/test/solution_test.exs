5f3251f3c312097d3dddf9d09809e4a3273569c0
Yuriy Zhloba <y_zhloba@wargaming.net>
2021-05-29 11:04:06 +0300

    refactor course structure

--- modules/10-basics/60-match-operator/test/solution_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule Test do
-  use ExUnit.Case
-  import ExUnit.CaptureIO
-
-  test "greets the world" do
-    msg = capture_io(fn -> Solution.hello() end) |> String.trim()
-    assert msg == "Hello, World!"
-  end
-end
