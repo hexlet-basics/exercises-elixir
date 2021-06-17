82466b8cabcb38ccba5cc7f6d3c8915d37abcaff
Yuriy Zhloba <y_zhloba@wargaming.net>
2021-06-17 16:00:19 +0300

    data-types/map exercise

--- modules/20-data-types/40-maps/lib/solution.ex
+++ modules/20-data-types/40-maps/lib/solution.ex
@@ -1,5 +1,16 @@
 defmodule Solution do
-  def hello do
-    IO.puts("Hello, World!")
+
+  def keys_sum(map, key1, key2) do
+    Map.get(map, key1, 0) + Map.get(map, key2, 0)
   end
+
+  def keys_product(map, key1, key2) do
+    Map.get(map, key1, 1) * Map.get(map, key2, 1)
+  end
+
+  def copy_key(from_map, to_map, key, default_value) do
+    value = Map.get(from_map, key, default_value)
+    Map.put(to_map, key, value)
+  end
+  
 end
--- modules/20-data-types/40-maps/test/solution_test.exs
+++ modules/20-data-types/40-maps/test/solution_test.exs
@@ -1,9 +1,41 @@
 defmodule Test do
   use ExUnit.Case
-  import ExUnit.CaptureIO
+  import Solution
 
-  test "greets the world" do
-    msg = capture_io(fn -> Solution.hello() end) |> String.trim()
-    assert msg == "Hello, World!"
+  test "keys_sum" do
+    map = %{a: 1, b: 2, c: 42}
+    assert keys_sum(map, :a, :b) == 3
+    assert keys_sum(map, :a, :c) == 43
+    assert keys_sum(map, :c, :b) == 44
+    assert keys_sum(map, :a, :a) == 2
+    assert keys_sum(map, :a, :d) == 1
+    assert keys_sum(map, :d, :c) == 42
+    assert keys_sum(map, :d, :e) == 0
   end
+
+  test "keys_product" do
+    map = %{one: 1, five: 5, ten: 10}
+    assert keys_product(map, :one, :five) == 5
+    assert keys_product(map, :one, :ten) == 10
+    assert keys_product(map, :five, :ten) == 50
+    assert keys_product(map, :five, :five) == 25
+    assert keys_product(map, :five, :two) == 5
+    assert keys_product(map, :two, :ten) == 10
+    assert keys_product(map, :two, :three) == 1
+  end
+
+  test "copy_key" do
+    map1 = %{a: 1, b: 2}
+    map2 = %{c: 3, d: 4}
+    assert copy_key(map1, map2, :a, 42) == %{c: 3, d: 4, a: 1}
+    assert copy_key(map1, map2, :b, 42) == %{c: 3, d: 4, b: 2}
+    assert copy_key(map1, map2, :e, 42) == %{c: 3, d: 4, e: 42}
+    assert copy_key(map1, map2, :c, 42) == %{c: 42, d: 4}
+    
+    assert copy_key(map2, map1, :c, 42) == %{a: 1, b: 2, c: 3}
+    assert copy_key(map2, map1, :d, 42) == %{a: 1, b: 2, d: 4}
+    assert copy_key(map2, map1, :e, 42) == %{a: 1, b: 2, e: 42}
+    assert copy_key(map2, map1, :b, 42) == %{a: 1, b: 42}
+  end
+  
 end
