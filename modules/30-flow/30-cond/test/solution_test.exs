c2b9457ac9d82f23d2d9c17f3aeea49449f825b7
Yuriy Zhloba <y_zhloba@wargaming.net>
2021-11-04 20:59:07 +0300

    flow/cond exercise

--- modules/30-flow/30-cond/test/solution_test.exs
+++ modules/30-flow/30-cond/test/solution_test.exs
@@ -1,9 +1,23 @@
 defmodule Test do
   use ExUnit.Case
-  import ExUnit.CaptureIO
+  import Solution
 
-  test "greets the world" do
-    msg = capture_io(fn -> Solution.hello() end) |> String.trim()
-    assert msg == "Hello, World!"
+  test "single_win? test" do
+    assert single_win?(true, false)
+    assert single_win?(false, true)
+    assert not single_win?(true, true)
+    assert not single_win?(false, false)
   end
+
+  test "double_win? test" do
+    assert :ab == double_win?(true, true, false)
+    assert :bc == double_win?(false, true, true)
+    assert :ac == double_win?(true, false, true)
+    assert not double_win?(true, true, true)
+    assert not double_win?(false, false, false)
+    assert not double_win?(true, false, false)
+    assert not double_win?(false, true, false)
+    assert not double_win?(false, false, true)
+  end
+
 end
