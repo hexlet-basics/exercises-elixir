33bd4fd618abdb01fbdfe19a0cf348ae5aa4bf88
Yuriy Zhloba <y_zhloba@wargaming.net>
2021-11-04 20:33:22 +0300

    flow/case excersise

--- modules/30-flow/20-case/lib/solution.ex
+++ modules/30-flow/20-case/lib/solution.ex
@@ -1,5 +1,20 @@
 defmodule Solution do
-  def hello do
-    IO.puts("Hello, World!")
+
+  def join_game(user) do
+    case user do
+      {:user, _name, _age, :admin} -> :ok
+      {:user, _name, _age, :moderator} -> :ok
+      {:user, _name, age, _role} when age >= 18 -> :ok
+      _ -> :error
+    end
   end
+
+  def move_allowed?(current_color, figure) do
+    case figure do
+      {:pawn, ^current_color} -> true
+      {:rock, ^current_color} -> true
+      _ -> false
+    end
+  end
+
 end
