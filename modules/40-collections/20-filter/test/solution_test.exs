c652cdabf05cc15ed79eb95b738f10863b6f71a0
solar05 <artem00298@gmail.com>
2023-07-07 02:45:15 +0300

    Add filter exercise

--- modules/40-collections/20-filter/test/solution_test.exs
+++ modules/40-collections/20-filter/test/solution_test.exs
@@ -1,9 +1,9 @@
 defmodule Test do
   use ExUnit.Case
-  import ExUnit.CaptureIO
 
-  test "greets the world" do
-    msg = capture_io(fn -> Solution.hello() end) |> String.trim()
-    assert msg == "Hello, World!"
+  test "inc_numbers work" do
+    assert Solution.inc_numbers(["foo", false, ["foo"]]) == []
+    assert Solution.inc_numbers([10, "foo", false, true, ["foo"], 1.2, %{}, 32]) == [11, 2.2, 33]
+    assert Solution.inc_numbers([1, 2, 3, 4, 5, 6.0]) == [2, 3, 4, 5, 6, 7.0]
   end
 end
