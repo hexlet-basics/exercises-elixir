7f6b3e9a5c632cc1fc34a385a09aa4b252831049
solar05 <artem00298@gmail.com>
2023-07-08 02:52:39 +0300

    Add comprehension exercise

--- modules/40-collections/40-comprehension/test/solution_test.exs
+++ modules/40-collections/40-comprehension/test/solution_test.exs
@@ -1,9 +1,62 @@
 defmodule Test do
   use ExUnit.Case
-  import ExUnit.CaptureIO
 
-  test "greets the world" do
-    msg = capture_io(fn -> Solution.hello() end) |> String.trim()
-    assert msg == "Hello, World!"
+  test "fetch_gamers work" do
+    assert Solution.fetch_gamers([]) == []
+
+    employees = [
+      %{
+        name: "Eric",
+        status: :active,
+        hobbies: [%{name: "Text Adventures", type: :gaming}, %{name: "Chickens", type: :animals}]
+      },
+      %{
+        name: "Mitch",
+        status: :former,
+        hobbies: [%{name: "Woodworking", type: :making}, %{name: "Homebrewing", type: :making}]
+      },
+      %{
+        name: "Greg",
+        status: :active,
+        hobbies: [
+          %{name: "Dungeons & Dragons", type: :gaming},
+          %{name: "Woodworking", type: :making}
+        ]
+      }
+    ]
+
+    assert Solution.fetch_gamers(employees) == [
+             {"Eric", %{name: "Text Adventures", type: :gaming}},
+             {"Greg", %{name: "Dungeons & Dragons", type: :gaming}}
+           ]
+
+    employees = [
+      %{
+        name: "Eric",
+        status: :former,
+        hobbies: [%{name: "Text Adventures", type: :gaming}, %{name: "Chickens", type: :animals}]
+      },
+      %{
+        name: "Alice",
+        status: :active,
+        hobbies: [
+          %{name: "World of Warcraft", type: :gaming}
+        ]
+      }
+    ]
+
+    assert Solution.fetch_gamers(employees) == [
+             {"Alice", %{name: "World of Warcraft", type: :gaming}}
+           ]
+
+    employees = [
+      %{
+        name: "Mitch",
+        status: :former,
+        hobbies: [%{name: "Woodworking", type: :making}, %{name: "Homebrewing", type: :making}]
+      }
+    ]
+
+    assert Solution.fetch_gamers(employees) == []
   end
 end
