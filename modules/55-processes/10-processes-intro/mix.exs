f36442f7808d9d3f25a4e844ebd4875eede7c1a8
solar05 <artem00298@gmail.com>
2023-07-09 14:08:46 +0300

    Add other modules

---
+++ modules/55-processes/10-processes-intro/mix.exs
@@ -0,0 +1,11 @@
+defmodule Exercise.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :exercise,
+      version: "0.1.0",
+    ]
+  end
+end
+
---
+++ modules/60-macros/10-macros-intro/test/solution_test.exs
@@ -0,0 +1,7 @@
+defmodule Test do
+  use ExUnit.Case
+
+  test "hello world" do
+    assert Solution.hello_world() == :hello_world
+  end
+end
