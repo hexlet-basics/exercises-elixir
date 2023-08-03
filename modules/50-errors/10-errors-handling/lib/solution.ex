4ccb86113d15dfa4114146a4d55c9491bbcbb5ff
solar05 <artem00298@gmail.com>
2023-08-03 18:15:03 +0300

    Add exceptions exercise

--- modules/50-errors/10-errors-handling/lib/solution.ex
+++ modules/50-errors/10-errors-handling/lib/solution.ex
@@ -1,6 +1,11 @@
 defmodule Solution do
   # BEGIN
-  def hello_world, do: :hello_world
+  def my_div(x, y) when y == 0,
+    do: raise(ArgumentError, message: "Divide #{x} by zero is prohibited!")
+
+  def my_div(x, y) do
+    Integer.floor_div(x, y)
+  end
 
   # END
 end
--- modules/50-errors/10-errors-handling/test/solution_test.exs
+++ modules/50-errors/10-errors-handling/test/solution_test.exs
@@ -1,7 +1,16 @@
 defmodule Test do
   use ExUnit.Case
 
-  test "hello world" do
-    assert Solution.hello_world() == :hello_world
+  test "my_div work" do
+    assert_raise(ArgumentError, "Divide 10 by zero is prohibited!", fn ->
+      Solution.my_div(10, 0)
+    end)
+
+    assert_raise(ArgumentError, "Divide 128 by zero is prohibited!", fn ->
+      Solution.my_div(128, 0)
+    end)
+
+    assert Solution.my_div(128, 2) == 64
+    assert Solution.my_div(0, 2) == 0
   end
 end
