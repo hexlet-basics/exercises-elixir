c9e5078f97d91a1f6c583fcfa3af80c2aa85c1e8
Artem Solomatin <artem00298@gmail.com>
2023-09-26 23:43:13 +0300

    Fix flaky tests

--- modules/55-processes/60-genservers/test/solution_test.exs
+++ modules/55-processes/60-genservers/test/solution_test.exs
@@ -6,42 +6,48 @@ defmodule Test do
       {:ok, pid} = Solution.start_link()
 
       assert Process.alive?(pid)
+      Process.exit(pid, :normal)
     end
 
     test "current_state work" do
-      {:ok, _} = Solution.start_link(%{test: "value"})
+      {:ok, pid} = Solution.start_link(%{test: "value"})
 
       assert Solution.current_state() == %{test: "value"}
+      Process.exit(pid, :normal)
     end
 
     test "reset work" do
-      {:ok, _} = Solution.start_link(%{test: "value"})
+      {:ok, pid} = Solution.start_link(%{test: "value"})
 
       assert Solution.reset() == :ok
       assert Solution.current_state() == %{}
+      Process.exit(pid, :normal)
     end
 
     test "has? work" do
-      {:ok, _} = Solution.start_link(%{test: "value"})
+      {:ok, pid} = Solution.start_link(%{test: "value"})
 
       assert Solution.has?(:test)
       refute Solution.has?(:some)
+      Process.exit(pid, :normal)
     end
 
     test "add work" do
-      {:ok, _} = Solution.start_link(%{test: "value"})
+      {:ok, pid} = Solution.start_link(%{test: "value"})
 
       assert Solution.add(:some, 2) == :ok
       assert Solution.current_state() == %{test: "value", some: 2}
+      Process.exit(pid, :normal)
     end
 
     test "drop work" do
-      {:ok, _} = Solution.start_link(%{test: "value"})
+      {:ok, pid} = Solution.start_link(%{test: "value"})
 
       assert Solution.drop(:some) == :ok
       assert Solution.current_state() == %{test: "value"}
       assert Solution.drop(:test) == :ok
       assert Solution.current_state() == %{}
+      Process.exit(pid, :normal)
     end
   end
 end
