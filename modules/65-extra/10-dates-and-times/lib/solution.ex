ece8d66cdcd9caea3a80da63200fe73953ff076d
Соломатин Артем Николаевич <artem00298@gmail.com>
2023-11-18 18:55:28 +0300

    Fix

--- modules/65-extra/10-dates-and-times/lib/solution.ex
+++ modules/65-extra/10-dates-and-times/lib/solution.ex
@@ -7,11 +7,11 @@ defmodule Solution do
   def shift_days(time, 0), do: time
 
   def shift_days(%Time{} = time, amount) when is_integer(amount) do
-    Time.add(time, amount * 24, :hour)
+    Time.add(time, days_to_seconds(amount) * 24, :second)
   end
 
   def shift_days(%NaiveDateTime{} = time, amount) when is_integer(amount) do
-    NaiveDateTime.add(time, amount, :day)
+    NaiveDateTime.add(time, days_to_seconds(amount), :second)
   end
 
   def shift_days(%Date{} = time, amount) when is_integer(amount) do
@@ -19,7 +19,11 @@ defmodule Solution do
   end
 
   def shift_days(%DateTime{} = time, amount) when is_integer(amount) do
-    DateTime.add(time, amount, :day)
+    DateTime.add(time, days_to_seconds(amount), :second)
+  end
+
+  defp days_to_seconds(amount) do
+    amount * 24 * 60 * 60
   end
 
   # END
