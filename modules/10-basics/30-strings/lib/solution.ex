5f65b156b99b5522dd0ac696b53591ffb846e9a7
Соломатин Артем Николаевич <artem00298@gmail.com>
2023-11-27 17:42:23 +0300

    Update naming

--- modules/10-basics/30-strings/lib/solution.ex
+++ modules/10-basics/30-strings/lib/solution.ex
@@ -1,6 +1,6 @@
 defmodule Solution do
   # BEGIN
-  def concate_and_upcase(str1, str2) do
+  def join_and_upcase(str1, str2) do
     String.trim(String.upcase(str1 <> str2, :ascii))
   end
 
--- modules/10-basics/30-strings/test/solution_test.exs
+++ modules/10-basics/30-strings/test/solution_test.exs
@@ -1,14 +1,14 @@
 defmodule Test do
   use ExUnit.Case
 
-  test "concate and upcase" do
-    res = Solution.concate_and_upcase(" привет ", "world!")
+  test "join and upcase works" do
+    res = Solution.join_and_upcase(" привет ", "world!")
     assert res == "привет WORLD!"
 
-    res = Solution.concate_and_upcase("hello ", "мир!  ")
+    res = Solution.join_and_upcase("hello ", "мир!  ")
     assert res == "HELLO мир!"
 
-    res = Solution.concate_and_upcase("   hello ", "world!  ")
+    res = Solution.join_and_upcase("   hello ", "world!  ")
     assert res == "HELLO WORLD!"
   end
 end
